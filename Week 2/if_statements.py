9320a21d1b4936e76fbabaef9bb4993a6bc39e68
Sebastian <2ciobg75@solent.ac.uk>
2023-10-12 21:13:05 +0100

    Added code to demonstrate the use of an if statement.

---
+++ Week 2/if_statements.py
@@ -0,0 +1,9 @@
+# Ask the user for the type of book
+print("What type of book is this?")
+book = input()
+
+# Check to see if the user picked adventure
+if book == "adventure":
+    print("I like adventure books!")
+
+print("Finished reading book.")
