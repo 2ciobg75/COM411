a840c78fe4fe7cd00671220af1e5b6097e2bf15a
Sebastian <2ciobg75@solent.ac.uk>
2023-10-12 20:09:26 +0100

    Added code to demonstrate the use of different data types.

--- input_task.py
+++ input_task.py
@@ -1,4 +1,21 @@
 #Ask user to enter their name
 print("What is your name?")
-name = input()
-print(f"It is nice to meet you {name}")
+name = str(input())
+
+#Ask the user for their age
+print("How old are you (in years)?")
+age = int(input())
+
+#Ask the user for their height
+print("How tall are you (in meters)?")
+height = float(input())
+
+#Ask the user for their weight
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
+
+#Calculation of BMI
+BMI = weight/height**2
+
+#Display the result
+print(f"{name} you are {age} years old and you BMI is {BMI:.2f}")
