0b516e299573374aba38bfc231530ff3641fc6db
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-10-13 14:34:33 +0100

    Added code to demonstrate the use of a while loop with a counter.

--- Week 3/while_loops.py
+++ Week 3/while_loops.py
@@ -8,3 +8,14 @@ while apples_numb != 0:
     apples_numb = apples_numb - 1
     print("Removed one apple.")
 
+# Ask the user how many obstacles should the robot avoid
+print("How many obstacles must I avoid?")
+obstacles_numb = int(input())
+obstacles_var = 0
+
+# Count the number of obstacles avoided
+print(f"I will avoid {obstacles_numb} obstacles!")
+while obstacles_numb != obstacles_var:
+    obstacles_var = obstacles_var + 1
+    print(f"Avoiding...Done! {obstacles_var} obstacles avoided.")
+print("\n All obstacles have been avoided")
