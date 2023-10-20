a5cb09a59994afa9025ef6f66f7af5889544250d
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-10-20 15:03:53 +0100

    Added code to demonstrate the use of built-in functions:abs,range,chr.

--- Week 4/built_in_functions.py
+++ Week 4/built_in_functions.py
@@ -6,8 +6,22 @@ letter = input()
 
 # Check to see if the user entered one character
 if len(letter) == 1:
-    print(f"The ASCII code for {letter} is {ord(letter)}")
+    print(f"The ASCII code for {letter} is: {ord(letter)}")
 else:
     print("You need to enter one character")
+
+print("Program Ended!")
+
+# Ask the user to enter some ASCII code
+print("Program started!")
+
+print("Please enter an ASCII code: ")
+ascii_code = abs(int(input()))
+
+# Check to see if the ascii code is between 32 and 126
+if 32 <= ascii_code <= 126:
+    print(f"The character represented by ASCII code {ascii_code} is: {chr(ascii_code)}")
+else:
+    print("Please enter a ASCII code from 32 to 126.")
 
 print("Program Ended!")
