611d75e24bb6c4d40cd893baae9ba39da3c4a0b6
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-10-20 15:17:31 +0100

    Added code to demonstrate the use of a user-defined function.

---
+++ Week 4/custom_functions.py
@@ -0,0 +1,9 @@
+# Function that listens
+def listen():
+    print("What sound did you hear?")
+    sound = input()
+    print(f"That was a loud {sound}!")
+
+
+# Calling the function that listens
+listen()
