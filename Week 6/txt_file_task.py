cde6892529a5a88794a5cade7559cf8fb1af191a
Sebastian <2ciobg75@solent.ac.uk>
2023-11-03 14:55:58 +0000

    Added code to demonstrate how to read a text file.

--- Week 6/txt_file_task.py
+++ Week 6/txt_file_task.py
@@ -1,6 +1,7 @@
 import os
 
 
+# This function prints the current working directory
 def cwd():
     path = os.getcwd()
     print(f"The current working directory is {path}")
@@ -16,3 +17,43 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+print()
+
+
+# This function prints the first numbers of characters specified in the parameters
+def display_chars(file_path, numb_chars):
+    print(f"The first {numb_chars} characters are: ")
+    with open(file_path) as file:
+        data = file.read(numb_chars)
+        print(data)
+
+
+# This function displays the first line of the file
+def display_line(file_path):
+    print("The first line is: ")
+    with open(file_path) as file:
+        data = file.readline().strip()
+        print(data)
+
+
+# This function displays the whole file
+def display_text(file_path):
+    print(f"The full text is: ")
+    with open(file_path) as file:
+        data = file.read()
+        print(data)
+
+
+def run_task2():
+    display_chars("library.txt", 10)
+    print()
+    display_line("library.txt")
+    print()
+    display_text("library.txt")
+
+
+if __name__ == "__main__":
+    run_task2()
+
+print()
