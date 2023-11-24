63cd154538526b83b16c8737a945c81c25b8113d
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-11-24 15:15:39 +0000

    Updated classes human and robot with methods to display objects.

--- Week 9/human.py
+++ Week 9/human.py
@@ -10,6 +10,12 @@ class Human:
     def display(self):
         print(f"I am {self.name}")
 
+    def __repr__(self):
+        return f'human(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'Human {self.name} is {self.age} years old.'
+
 
 if __name__ == "__main__":
 
