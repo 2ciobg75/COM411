b07be312b564cce041db91834700c69d264c3070
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-12-01 14:45:41 +0000

    Added code to demonstrate how to read a CSV file.

---
+++ Week 6/csv_task.py
@@ -0,0 +1,22 @@
+import csv
+
+
+def read_csv(file_path):
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+
+        headings = next(csv_reader)
+        print(f"Headings:\n{headings}")
+
+        print("Values:")
+        for values in csv_reader:
+            print(values)
+
+
+def run_task1():
+    read_csv("clothing.csv")
+
+
+if __name__ == "__main__":
+    run_task1()
+
