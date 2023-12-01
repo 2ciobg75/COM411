4d7f4fa10f176879e5d65024d1761adf6192ee7a
Sebastian Ciobra <2ciobg75@solent.ac.uk>
2023-12-01 15:41:20 +0000

    Added code to export csv data.

--- Week 6/csv_task.py
+++ Week 6/csv_task.py
@@ -20,3 +20,51 @@ def run_task1():
 if __name__ == "__main__":
     run_task1()
 
+
+def extract(file_path):
+    print("Extracting...")
+
+    with open(file_path) as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        names = ""
+        for values in csv_reader:
+            names += f"{values[1]}\n"
+
+    print("Done!")
+    print(f"The extracted items are as follows:\n{names}")
+
+
+def run_task2():
+    extract("clothing.csv")
+
+
+if __name__ == "__main__":
+    run_task2()
+
+
+def export(file_path, number_items):
+    print("Exporting...")
+
+    with open(file_path, "a") as file:
+        for count in range(number_items):
+            print("Please enter the item id:")
+            item_id = int(input())
+
+            print("Please enter the item name:")
+            item_name = input()
+
+            print("Please enter the item colour:")
+            item_colour = input()
+
+            data = f"{item_id},{item_name},{item_colour}\n"
+            file.write(data)
+    print("Done!")
+
+
+def run_task3():
+    export("exported_items.csv", 2)
+
+
+if __name__ == "__main__":
+    run_task3()
